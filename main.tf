03ed49c79d42507430c6163c911491826012f55b
Vitanet-tech <vitalisibekwe@gmail.com>
2022-08-07 12:32:29 +0100

    Correction commit

--- main.tf
+++ main.tf
@@ -11,29 +11,37 @@ terraform {
 #Configure the Microsoft Azure Provider
 provider "azurerm" {
     features {}
-  
 }
 
 #Create a resource group
 resource "azurerm_resource_group" "myrg" {
     name = "terraformRG"
-    location = "UK south" 
+    location = "UK south"
 }
 
 #Create a virtual network within the resource group
 resource "azurerm_virtual_network" "myvnet" {
-    name = "terraformVnet"
+    name = "terraform-vnet"
     resource_group_name = azurerm_resource_group.myrg.name
     location = azurerm_resource_group.myrg.location
-    address_space = [ "10.0.0.0/16" ]
-  
+    address_space = [ "10.0.0.0/16" ] 
 }
 
 #Create a subnet within the vnet
 resource "azurerm_subnet" "mysubnet" {
-    name = "terraformSubnet"
+    name = "terraform-subnet"
     resource_group_name = azurerm_resource_group.myrg.name
     virtual_network_name = azurerm_virtual_network.myvnet.name
-    address_space = [ "10.0.0.1/24" ]
-  
-}
+    address_prefixes = [ "10.0.0.0/24" ]
+  }
+
+
+#Create a Storage within the resource group
+resource "azurerm_storage_account" "vitastore24" {
+    name = "vitastore24"
+    resource_group_name = azurerm_resource_group.myrg.name
+    location = azurerm_resource_group.myrg.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    allow_blob_public_access = true
+  }
